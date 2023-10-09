d36ed366a89235d11cff9ce872f24a73910100be
Mike Tran <hoang.tran4@sotatek.com>
2023-10-09 16:41:52 +0700

    mike: update not found page

---
+++ src/app/homepage/page.tsx
@@ -0,0 +1,11 @@
+interface HomePageProps {}
+
+const HomePage: React.FC<HomePageProps> = () => {
+  return (
+    <div>
+      <p>This is homepage</p>
+    </div>
+  );
+};
+
+export default HomePage;
