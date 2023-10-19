5f32b6304d2c24030725612887b27cc1f7a1217b
Mike Tran <hoang.tran4@sotatek.com>
2023-10-19 17:01:10 +0700

    mike: wip

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,17 +3,21 @@
 import React from 'react';
 
 import Image from 'next/image';
+import Link from 'next/link';
 
 import styles from './page.module.scss';
 
 import Header from '@/app/components/Header/Header';
 import GoogleLogoImg from 'public/images/google-logo.svg';
-import Link from 'next/link';
+import ShowPasswordImg from 'public/images/show-password.svg';
+import HidePasswordImg from 'public/images/hide-password.svg';
 
 interface SignUpProps {}
 
 const SignUp: React.FC<SignUpProps> = () => {
   const [error, setError] = React.useState<Boolean>(false);
+  const [showPassword, setShowPassword] = React.useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = React.useState(false);
 
   const handleClick = () => {
     return alert('Not implemented');
@@ -21,6 +25,14 @@ const SignUp: React.FC<SignUpProps> = () => {
 
   const handleSubmit = () => {
     return setError(true);
+  };
+
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleToggleConfirmPassword = () => {
+    setShowConfirmPassword(!showConfirmPassword);
   };
 
   return (
@@ -42,34 +54,41 @@ const SignUp: React.FC<SignUpProps> = () => {
 
             <div className={styles.signupForm}>
               <div className={styles.firstSection}>
-                <label htmlFor='firstName'>First Name:</label>
-                <input
-                  type='text'
-                  id='firstName'
-                  name='firstName'
-                  required
-                  placeholder='First Name'
-                  className={styles.input}
-                />
+                <div style={{ position: 'relative' }}>
+                  <label htmlFor='firstName'>First Name:</label>
+                  <input
+                    type='text'
+                    id='firstName'
+                    name='firstName'
+                    required
+                    placeholder='First Name'
+                    className={styles.input}
+                  />
+                  {error ? <span className={styles.errorField}>Enter your first name</span> : null}
+                </div>
 
-                <label htmlFor='lastName'>Last Name:</label>
-                <input
-                  type='text'
-                  id='lastName'
-                  name='lastName'
-                  required
-                  placeholder='Last Name'
-                  className={styles.input}
-                />
+                <div style={{ position: 'relative' }}>
+                  <label htmlFor='lastName'>Last Name:</label>
+                  <input
+                    type='text'
+                    id='lastName'
+                    name='lastName'
+                    required
+                    placeholder='Last Name'
+                    className={styles.input}
+                  />
+                  {error ? <span className={styles.errorField}>Enter your last name</span> : null}
+                </div>
               </div>
 
               <label htmlFor='email'>Email:</label>
               <input type='email' id='email' name='email' required placeholder='Email' className={styles.input} />
+              {error ? <span className={styles.errorField}>Invalid email</span> : null}
 
-              <div>
+              <div className={styles.secondSection}>
                 <label htmlFor='password'>Password:</label>
                 <input
-                  type='password'
+                  type={showPassword ? 'text' : 'password'}
                   id='password'
                   name='password'
                   required
@@ -77,8 +96,27 @@ const SignUp: React.FC<SignUpProps> = () => {
                   className={`${styles.input} ${error ? styles.error : ''}`}
                 />
                 {error ? (
-                  <span className={styles.passwordError}>Passwords must be at least 8 characters long.</span>
+                  <span className={styles.errorField}>Passwords must be at least 8 characters long.</span>
                 ) : null}
+                <button onClick={handleTogglePassword} type='button'>
+                  <Image src={showPassword ? HidePasswordImg : ShowPasswordImg} alt='Show Password' />
+                </button>
+              </div>
+
+              <div className={styles.secondSection}>
+                <label htmlFor='confirm-password'>Password:</label>
+                <input
+                  type={showConfirmPassword ? 'text' : 'password'}
+                  id='confirm-password'
+                  name='confirm-password'
+                  required
+                  placeholder='Confirm Password'
+                  className={`${styles.input} ${error ? styles.error : ''}`}
+                />
+                {error ? <span className={styles.errorField}>Password must be confirmed</span> : null}
+                <button onClick={handleToggleConfirmPassword} type='button'>
+                  <Image src={showConfirmPassword ? HidePasswordImg : ShowPasswordImg} alt='Show Password' />
+                </button>
               </div>
             </div>
 
