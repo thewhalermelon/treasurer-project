c25ae02bdb129ed3712eac8a8f07d074f20bb5a9
Mike Tran <hoang.tran4@sotatek.com>
2023-10-26 17:52:07 +0700

    Mike: revert code

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const path = require('path');
 const nextConfig = {
   images: {
     domains: ['cdn.treasurer.co.kr', 'treasurer-source.s3.ap-northeast-2.amazonaws.com'],
-    formats: ['image/avif', 'image/webp'],
   },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
